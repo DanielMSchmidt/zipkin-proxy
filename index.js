9d40f86a9dedd3d338038face20c2311ec28e517
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-12-12 00:03:32 +0100

    complete rewrite of logic without own span

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const proxy = require('http-proxy-middleware');
 const {Tracer, ExplicitContext, BatchRecorder} = require('zipkin');
 const {HttpLogger} = require('zipkin-transport-http');
 const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
+const fetch = require('node-fetch');
 
 const localServiceName = process.env.ZIPKIN_SERVICE_NAME || 'proxy';
 const ctxImpl = new ExplicitContext();
@@ -24,6 +25,35 @@ if (!process.env.ENDPOINT) {
 
 const tracer = new Tracer({ctxImpl, recorder, localServiceName});
 app.use(zipkinMiddleware({tracer}));
-app.use('/', proxy({target: process.env.ENDPOINT, changeOrigin: true}));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-B3-SpanId, X-B3-ParentSpanId, X-B3-Sampled, X-B3-TraceId");
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200);
+    res.end();
+  } else {
+    console.log('LETS DO THIS!');
+    fetch(process.env.ENDPOINT + '/' + req.originalUrl, {
+      method: req.method,
+    }).then(response => {
+      response.json().then(body => {
+        console.log('R', body);
+        res.status(response.status);
+        res.send(response.body);
+  
+        res.end();
+      });
+    }, error => {
+      console.log('ERR', error);
+      // res.send(error);
+      res.status(200);
+      res.end();
+    })
+  }
+});
 
-app.listen(80, () => console.log('Starting server on port 80'));
+
+// app.get('/', proxy({ target: process.env.ENDPOINT, logLevel: 'debug'}));
+
+app.listen(4444, () => console.log('Starting server on port 80'));
